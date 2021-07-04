e3748ff5c096b9f35b987a5951cf9835120a796c
thomasgermain07 <thomasgermain07@hotmail.fr>
2021-07-04 19:40:23 +0200

    Html css imporvement

--- www/client/src/components/FriendWindow.vue
+++ www/client/src/components/FriendWindow.vue
@@ -1,23 +1,59 @@
 <template>
   <a class="open-button" v-if="!open" @click="toggle_window">
-    <i class="fas fa-bars fa-2x"></i>
+    <!-- <i class="fas fa-bars fa-2x"></i> -->
+    <i class="fas fa-comments fa-2x"></i>
   </a>
   <div v-if="open" class="window">
     <header class="top-bar">
       <a @click="toggle_window">
-        <i class="far fa-times-circle"></i>
+        <i class="far fa-times-circle close-button"></i>
       </a>
     </header>
+
     <div class="friend-list">
-      <ul>
-        <li v-for="friend in sortedFriends" :key="friend">
-          <div class="friend">
-            {{ friend.nickname }}
-            <i v-if="friend.connected" class="fas fa-circle connected"></i>
-            <i v-if="!friend.connected" class="fas fa-circle disconnected"></i>
-          </div>
-        </li>
-      </ul>
+      <a
+        class="roll-menu"
+        :class="{ 'roll-menu--open': showOnline }"
+        @click="toggle_online"
+      >
+        Online
+        <i class="far fa-arrow-alt-circle-down arrow"></i>
+      </a>
+
+      <div class="friend-container" v-if="showOnline">
+        <ul>
+          <li
+            v-for="friend in onlineFriends"
+            :key="friend"
+            @click.right.prevent="right_click"
+          >
+            <div class="friend">
+              {{ friend.nickname }}
+              <i class="fas fa-circle status status--connected"></i>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <a
+        class="roll-menu"
+        :class="{ 'roll-menu--open': showOffline }"
+        @click="toggle_offline"
+      >
+        Offline
+        <i class="far fa-arrow-alt-circle-down arrow"></i>
+      </a>
+
+      <div class="friend-container" v-if="showOffline">
+        <ul>
+          <li v-for="friend in offlineFriends" :key="friend">
+            <div class="friend">
+              {{ friend.nickname }}
+              <i class="fas fa-circle status status--disconnected"></i>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -31,22 +67,30 @@ interface Friend {
 }
 
 export default {
+  data() {
+    return {
+      open: false,
+      showOnline: true,
+      showOffline: false,
+    }
+  },
   setup() {
     let friends = ref<Friend[]>()
     let loading = ref(true)
-    let open = ref(false)
 
-    const getFriends = () => {
-      axios
-        .get('https://60d5fd1b943aa60017768d55.mockapi.io/api/users') // TODO : Link friend list api
-        .then((res) => {
-          friends.value = res.data
-          loading.value = false
-        })
+    const getFriends = async () => {
+      const { data } = await axios.get(
+        'https://60d5fd1b943aa60017768d55.mockapi.io/api/users',
+      )
+      friends.value = data
+      loading.value = false
     }
 
-    const sortedFriends = computed(() => {
+    const onlineFriends = computed(() => {
       return friends.value?.filter((friend) => friend.connected)
+    })
+    const offlineFriends = computed(() => {
+      return friends.value?.filter((friend) => !friend.connected)
     })
 
     onMounted(getFriends)
@@ -54,14 +98,26 @@ export default {
     return {
       friends,
       loading,
-      open,
       getFriends,
-      sortedFriends,
+      onlineFriends,
+      offlineFriends,
     }
   },
   methods: {
     toggle_window() {
       this.open = !this.open
+      if (this.open) {
+        this.getFriends()
+      }
+    },
+    toggle_online() {
+      this.showOnline = !this.showOnline
+    },
+    toggle_offline() {
+      this.showOffline = !this.showOffline
+    },
+    right_click() {
+      console.log('right clicked')
     },
   },
 }
@@ -72,6 +128,7 @@ export default {
   position: fixed;
   bottom: 0;
   right: 10px;
+  margin: 5px;
 }
 
 .window {
@@ -90,7 +147,7 @@ export default {
 
 .friend-list {
   height: 275px;
-  overflow-y: scroll;
+  overflow-y: auto;
   overflow-x: hidden;
 }
 
@@ -99,32 +156,75 @@ export default {
   text-align: left;
   padding: 0.5em;
   margin: 0;
+  animation-name: slidein;
+  animation-duration: 0.2s;
 }
 
 .friend-list li {
   padding: 0.15em;
+  border-bottom: 1px solid darkgray;
+}
+
+.friend-list li:last-child {
+  border-bottom: none;
+}
+
+.roll-menu {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.3em;
+  cursor: pointer;
+  background-color: darkgrey;
+}
+
+.roll-menu--open {
+  border-bottom: 2px solid black;
+}
+
+.roll-menu .arrow {
+  transform: rotate(0deg);
+  transition: linear 0.2s;
+}
+
+.roll-menu--open .arrow {
+  transform: rotate(180deg);
+}
+
+.friend-container {
+  overflow: hidden;
 }
 
 .friend {
   position: relative;
 }
 
-.fa-times-circle {
+.close-button {
   color: whitesmoke;
 }
 
-.fa-circle {
+.status {
   position: absolute;
-  /* right: 0; */
+  right: 0;
   padding: 0.5em;
   font-size: 0.7em;
 }
 
-.connected {
+.status--connected {
   color: green;
 }
 
-.disconnected {
+.status--disconnected {
   color: red;
+}
+
+@keyframes slidein {
+  from {
+    transform: translateY(-100%);
+  }
+
+  to {
+    transform: translateY(0%);
+  }
 }
 </style>
--- www/client/src/components/NavigationBar.vue
+++ www/client/src/components/NavigationBar.vue
@@ -1,32 +1,24 @@
 <template>
-  <div>
-    <ul class="navigation">
-      <li class="left-section">
-        <router-link to="/" class="link">
-          <i class="fas fa-home"></i>
-        </router-link>
-      </li>
-      <li class="left-section">
-        <router-link to="/game" class="link">
-          <i class="fas fa-gamepad"></i>
-        </router-link>
-      </li>
-      <li class="left-section">
-        <router-link to="/users" class="link">
-          <i class="fas fa-users"></i>
-        </router-link>
-      </li>
-      <li class="right-section">
-        <router-link to="/logout" class="link">
-          <i class="fas fa-sign-out-alt"></i>
-        </router-link>
-      </li>
-      <li class="right-section">
-        <router-link to="/profile" class="link">
-          <i class="fas fa-user"></i>
-        </router-link>
-      </li>
-    </ul>
+  <div class="topbar">
+    <div class="navigation">
+      <router-link to="/" class="link">
+        <i class="fas fa-home"></i>
+      </router-link>
+      <router-link to="/game" class="link">
+        <i class="fas fa-gamepad"></i>
+      </router-link>
+      <router-link to="/users" class="link">
+        <i class="fas fa-users"></i>
+      </router-link>
+    </div>
+    <div class="navigation">
+      <router-link to="/profile" class="link">
+        <i class="fas fa-user"></i>
+      </router-link>
+      <router-link to="/logout" class="link">
+        <i class="fas fa-sign-out-alt"></i>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -35,26 +27,21 @@ export default {}
 </script>
 
 <style>
-.navigation {
+.topbar {
+  display: flex;
+  justify-content: space-between;
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
   background-color: grey;
 }
-.left-section {
-  float: left;
-}
 
-.right-section {
-  float: right;
+.navigation {
+  display: flex;
 }
 
 .link {
-  display: block;
   color: white;
   padding: 8px;
   text-align: center;
