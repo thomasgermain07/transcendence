87da4a6a155b813d94135ae83e2753ce71e5bf25
vmaokham42 <vmaokham@student.42.fr>
2021-07-21 15:39:59 +0200

    Backend cleanup - game rooms locking system upgraded

--- www/server/src/game/players/subscribers/players.subscribers.ts
+++ www/server/src/game/players/subscribers/players.subscribers.ts
@@ -9,6 +9,7 @@ import {
 import { Player } from '../entities/player.entity';
 
 import { RoomsService } from '../../rooms/services/rooms.service';
+import { GameState } from '../../rooms/entities/room.entity';
 
   
   @EventSubscriber()
@@ -25,17 +26,17 @@ import { RoomsService } from '../../rooms/services/rooms.service';
     }
     
     afterInsert(event: InsertEvent<Player>) {
-      console.log(`AFTER Player INSERTED: `, event.entity);
-      console.log('PLAYERS IN ROOM: ' + event.entity.room?.players.length)
       if (event.entity.room.players && event.entity.room.players.length == 1) {
         console.log('Locking Room ' + event.entity.room.id)
         this.roomsService.update(event.entity.room.id, {locked: true})
       }
     }
   
-    afterRemove(event: RemoveEvent<Player>) {
+    async afterRemove(event: RemoveEvent<Player>) {
       console.log(`AFT ENTITY WITH ID ${event.entityId} REMOVED: `, event.entity);
-      this.roomsService.update(event.entity.room.id, {locked: false})
+      if (event.entity.room.state === GameState.WAITING) {
+        await this.roomsService.update(event.entity.room.id, {locked: false})
+      }
     }
   
   }
