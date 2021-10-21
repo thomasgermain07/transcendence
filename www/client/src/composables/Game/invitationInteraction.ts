7cac58adcb82fd1aa41bdcec89fab65fa5d5d834
ThomasGermain-Macbook <thomasgermain07@hotmail.fr>
2021-10-21 10:41:06 +0200

    Front : Clean -> deleted few useless console.log

--- www/client/src/composables/Game/invitationInteraction.ts
+++ www/client/src/composables/Game/invitationInteraction.ts
@@ -53,7 +53,6 @@ export default function getInvitationInteraction() {
       let { data } = await axios.post('dm/accept-invitation', { ...invitation })
       return data
     } catch (e: AxiosErrType) {
-      console.log(`acceptInvitation : ${e.response.data.message}`)
       return e.response.data.message
     }
   }
